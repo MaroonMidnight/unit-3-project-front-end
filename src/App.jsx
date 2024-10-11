941e50b5f29ed6e9213f455132c197cad12156b6
christiangit <colvera1218@gmail.com>
2024-10-11 11:33:08 -0500

    fixed c omponent bugs and logging in

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState, createContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import Landing from './components/Landing/Landing';
-import Dashboard from './components/Dashboard/Dashboard';
-import SignupForm from './components/SignupForm/SignupForm';
-import SigninForm from './components/SigninForm/SigninForm';
-import * as authService from '../src/services/authService'; // import the authservice
+import NavBar from './components/NavBar/NavBar.jsx';
+import Landing from './components/Landing/Landing.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import SignupForm from './components/SignupForm/SignupForm.jsx';
+import SigninForm from './components/SigninForm/SigninForm.jsx';
+import * as authService from '../src/services/authService.js'; // import the authservice
 
 export const AuthedUserContext = createContext(null);
 
